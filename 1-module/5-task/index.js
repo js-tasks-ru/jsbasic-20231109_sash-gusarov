9fa078ed35ac9169a0c60a80bebc7e0548a0cb70
Alex <sash.gusarov@mail.ru>
2023-11-14 23:02:04 +0300

    решения модуль 1 задание 3, 4, 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + '…';
+  } else {
+    return str;
+  }
 }
+
+let truncateStr = truncate('Вот, что мне хотелось бы сказать на эту тему:', 20);
+console.log(truncateStr);
+let truncateStrSecond = truncate('Всем привет!', 20);
+console.log(truncateStrSecond);
+
