77666e293a01417439ed71223484e7aed2dea824
Alex <sash.gusarov@mail.ru>
2023-11-18 23:28:43 +0300

    решение модуль 2 задание 1,2,3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let totalSalary = 0;
+
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && isFinite(salaries[key])) {
+      totalSalary += salaries[key];
+    }
+  }
+  return totalSalary;
 }
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
+let result = sumSalary(salaries);
+console.log(result); 
+
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,11 @@
 function isEmpty(obj) {
-  // ваш код...
+  for (let key in obj) {
+    return false;
+  }
+  return true;
 }
+
+let schedule = {};
+alert(isEmpty(schedule));
+schedule["8:30"] = "подъём";
+alert(isEmpty(schedule));
