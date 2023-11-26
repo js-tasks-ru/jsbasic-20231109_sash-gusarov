fcb33f1e9a4feef5e911bdfdd11968605c24405f
Alex <sash.gusarov@mail.ru>
2023-11-26 12:58:56 +0300

    решение модуль 3 задачи 1-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let task = str.split(' ').join(',').split(',')
+  .filter(number => parseFloat(number))
+  .map(number => Number(number))
+
+  let result = {};
+  result.min = Math.min(...task);
+  result.max = Math.max(...task);
+  return result;
 }
